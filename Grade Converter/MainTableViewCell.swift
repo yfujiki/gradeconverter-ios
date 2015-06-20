b211217e239f9548b18e62ab1e92a308e36881ba
Yuichi Fujiki <yuichi@chaione.com>
2015-06-20 17:18:41 -0500

    Initial UI structure is implemented.

---
+++ Grade Converter/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  Grade Converter
+//
+//  Created by Yuichi Fujiki on 6/20/15.
+//  Copyright (c) 2015 Responsive Bytes. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Grade Converter/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainViewController.swift
+//  Grade Converter
+//
+//  Created by Yuichi Fujiki on 6/20/15.
+//  Copyright (c) 2015 Responsive Bytes. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.registerNib(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "MainTableViewCell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK:- UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MainTableViewCell") as! MainTableViewCell
+
+        return cell
+    }
+
+    // MARK:- UITableViewDelegate
+}
+
