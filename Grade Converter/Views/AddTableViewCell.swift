87b7130544a326a692c7ed78005d06d79b72bfda
yuichi fujiki <fjk89025@gmail.com>
2019-07-09 23:48:42 +0800

    Apply different size and corner radius for 'More' button.

--- Grade Converter/Views/AddTableViewCell.swift
+++ Grade Converter/Views/AddTableViewCell.swift
@@ -10,24 +10,47 @@ import UIKit
 
 class AddTableViewCell: UITableViewCell {
 
-    class var kCellHeight: CGFloat { return 96 }
+    class var kCellHeight: CGFloat {
+        if UIDevice.current.hasNotch {
+            return 120
+        } else {
+            return 96
+        }
+    }
 
-    @IBOutlet fileprivate weak var cardView: UIView!
+    @IBOutlet fileprivate weak var upperCardView: UIView!
+    @IBOutlet fileprivate weak var lowerCardView: UIView!
     @IBOutlet weak var label: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        cardView.layer.cornerRadius = 4
-        cardView.backgroundColor = UIColor.addColor()
+        upperCardView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        upperCardView.layer.cornerRadius = 4
+
+        if UIDevice.current.hasNotch {
+            lowerCardView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            lowerCardView.layer.cornerRadius = 36
+        } else {
+            lowerCardView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            lowerCardView.layer.cornerRadius = 4
+        }
+
+        [upperCardView, lowerCardView].forEach { cardView in
+            cardView?.backgroundColor = UIColor.addColor()
+        }
     }
 
     override func setHighlighted(_ highlighted: Bool, animated _: Bool) {
         if highlighted {
-            cardView.alpha = 0.8
+            [upperCardView, lowerCardView].forEach { cardView in
+                cardView.alpha = 0.8
+            }
             label.textColor = UIColor.gray
         } else {
-            cardView.alpha = 1.0
+            [upperCardView, lowerCardView].forEach { cardView in
+                cardView.alpha = 1.0
+            }
             label.textColor = UIColor.black
         }
     }
